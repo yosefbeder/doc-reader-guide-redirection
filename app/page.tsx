200974e9cad891adefdcf8743a3fe858746ff5b3
Yosef Beder <dryosefbeder@gmail.com>
2026-02-04 14:45:59 +0200

    Add detailed Arabic instructions

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,38 @@ import Logo from "@/public/logo.png";
 
 export default function Home() {
   return (
-    <main className="my-8 mx-auto max-w-96 flex flex-col gap-4 items-center text-center">
+    <main
+      dir="rtl"
+      className="my-8 mx-auto max-w-96 flex flex-col gap-4 items-center"
+    >
       <Image src={Logo} width={128} alt="Logo" suppressHydrationWarning />
       <span className="text-4xl font-extrabold text-cyan-700">
         DocReader Guide
       </span>
       <h1 className="text-2xl font-bold text-rose-600">🚨 Domain Changed!</h1>
-      <p className="text-slate-700">
-        The domain name of this web app has been changed to:
-      </p>
+      <p>تم تغيير اسم النطاق إلى:</p>
       <a
         href="https://doc-reader-guide.com/"
         className="inline-block underline text-cyan-600 hover:text-cyan-700 active:text-cyan-800 transition-colors"
       >
         https://doc-reader-guide.com/
       </a>
-      <p className="text-slate-700">
-        If you already have the app installed, please uninstall it first and
-        then reinstall it from the new domain.
-      </p>
+      <p>إذا كنت قمت بتثبيت البرنامج من قبل يرجى اتباع الخطوات الآتية:</p>
+      <ul className="list-decimal list-inside">
+        <li>قم بالضغط على الثلاث نقاط في أعلى اليمين أو اليسار</li>
+        <li>قم بالضغط على إلغاء التثبيت</li>
+        <li>
+          قم بزيارة{" "}
+          <a
+            href="https://doc-reader-guide.com/"
+            className="inline-block underline text-cyan-600 hover:text-cyan-700 active:text-cyan-800 transition-colors"
+          >
+            https://doc-reader-guide.com/
+          </a>
+        </li>
+        <li>قم بالضغط على الثلاث نقاط في أعلى اليمين أو اليسار</li>
+        <li>قم بالضغط على تثبيت</li>
+      </ul>
     </main>
   );
 }
